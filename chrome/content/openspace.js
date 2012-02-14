ecddc45638ab1bc168b640ef82057bd8b12336a0
slopjong <slopjong>
2012-02-14 03:37:50 +0100

    Running a timer.

--- chrome/content/openspace.js
+++ chrome/content/openspace.js
@@ -2,10 +2,16 @@ var OpenSpace = {
   onLoad: function() {
     // initialization code
     this.initialized = true;
+    setTimeout("OpenSpace.alert()", 2000);
   },
 
   onClick: function() {
     window.open("chrome://openspace/content/window.xul", "", "width=400,height=200,chrome");
+  },
+  
+  alert: function(){
+	alert('test');
+	setTimeout("OpenSpace.alert()", 2000);
   }
 };
 
