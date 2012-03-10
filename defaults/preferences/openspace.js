44be29e742cd14f17277b0b0b372b5f8bc8befe2
slopjong <slopjong>
2012-03-10 16:02:17 +0100

    Version increment and new default refresh interval is 90 seconds

--- defaults/preferences/openspace.js
+++ defaults/preferences/openspace.js
@@ -1,3 +1,3 @@
 pref("extensions.openspace.myspace", "Syn2cat" );
-pref("extensions.openspace.refresh_interval", 2 );
-pref("extensions.openspace.version", "0.2");
+pref("extensions.openspace.refresh_interval", 90 );
+pref("extensions.openspace.version", "0.3");
